97d711a695c3d3adf96d9dd815d3f0c17985eba5
Laurence Roberts <lsjroberts@gmail.com>
2013-08-14 15:54:24 +0100

    Move order item status service extension into share callback

--- src/Message/Mothership/OrderReturn/Bootstrap/Services.php
+++ src/Message/Mothership/OrderReturn/Bootstrap/Services.php
@@ -49,24 +49,28 @@ class Services implements ServicesInterface
 			return new OrderReturn\Edit($c['db.query'], $c['user'], $c['order.item.edit'], $c['order.refund.create']);
 		};
 
-		// Add basic item return statuses
-		$services['order.item.statuses']->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::AWAITING_RETURN, 'Awaiting Return'));
-		$services['order.item.statuses']->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::RETURN_REJECTED, 'Return Rejected'));
-		$services['order.item.statuses']->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::RETURN_ACCEPTED, 'Return Accepted'));
-		$services['order.item.statuses']->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::RETURN_RECEIVED, 'Return Received'));
+		$services['order.item.statuses'] = $services->share($services->extend('order.item.statuses', function($statuses) {
+			// Add basic item return statuses
+			$statuses->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::AWAITING_RETURN, 'Awaiting Return'));
+			$statuses->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::RETURN_REJECTED, 'Return Rejected'));
+			$statuses->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::RETURN_ACCEPTED, 'Return Accepted'));
+			$statuses->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::RETURN_RECEIVED, 'Return Received'));
 
-		// Add item exchange return statuses
-		$services['order.item.statuses']->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::AWAITING_RETURN_BALANCE_PAYMENT, 'Awaiting Return Balance Payment'));
-		$services['order.item.statuses']->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::RETURN_BALANCE_REFUNDED, 'Return Balance Refunded'));
-		$services['order.item.statuses']->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::AWAITING_EXCHANGE_DISPATCH, 'Awaiting Exchange Dispatch'));
-		$services['order.item.statuses']->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::RETURN_ITEM_EXCHANGED, 'Return Item Exchanged'));
-		$services['order.item.statuses']->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::EXCHANGE_DISPATCHED, 'Exchange Dispatched'));
+			// Add item exchange return statuses
+			$statuses->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::AWAITING_RETURN_BALANCE_PAYMENT, 'Awaiting Return Balance Payment'));
+			$statuses->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::RETURN_BALANCE_REFUNDED, 'Return Balance Refunded'));
+			$statuses->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::AWAITING_EXCHANGE_DISPATCH, 'Awaiting Exchange Dispatch'));
+			$statuses->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::RETURN_ITEM_EXCHANGED, 'Return Item Exchanged'));
+			$statuses->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::EXCHANGE_DISPATCHED, 'Exchange Dispatched'));
 
-		// Add item refund return statuses
-		$services['order.item.statuses']->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::REFUNDED, 'Refunded'));
+			// Add item refund return statuses
+			$statuses->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::REFUNDED, 'Refunded'));
 
-		// Add order return statuses
-		$services['order.statuses']->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::FULLY_RETURNED, 'Fully Returned'));
+			// Add order return statuses
+			$statuses->add(new Commerce\Order\Status\Status(OrderReturn\Statuses::FULLY_RETURNED, 'Fully Returned'));
+
+			return $statuses;
+		}));
 	}
 
 }
