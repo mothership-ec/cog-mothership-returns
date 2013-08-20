fef22acfbe97d36ca9c52fcdfd82373c9d8e9e64
Laurence Roberts <lsjroberts@gmail.com>
2013-08-20 10:39:01 +0100

    Fixed account routes double prefixing

--- src/Message/Mothership/OrderReturn/Bootstrap/AccountRoutes.php
+++ src/Message/Mothership/OrderReturn/Bootstrap/AccountRoutes.php
@@ -10,14 +10,14 @@ class AccountRoutes implements RoutesInterface
 	{
 		$router['ms.user.account']->setPrefix('/account/returns'); // temp
 
-		$router['ms.user.account']->add('ms.user.return.listing', '/returns', '::Controller:OrderReturn:Account:Listing#view');
-		$router['ms.user.account']->add('ms.user.return.detail', '/returns/view/{returnID}', '::Controller:OrderReturn:Account:Detail#view')
+		$router['ms.user.account']->add('ms.user.return.listing', '', '::Controller:OrderReturn:Account:Listing#view');
+		$router['ms.user.account']->add('ms.user.return.detail', '/view/{returnID}', '::Controller:OrderReturn:Account:Detail#view')
 			->setRequirement('returnID', '\d+');
 
-		$router['ms.user.account']->add('ms.user.return.create', '/returns/create/{itemID}', '::Controller:OrderReturn:Account:Create#view')
+		$router['ms.user.account']->add('ms.user.return.create', '/create/{itemID}', '::Controller:OrderReturn:Account:Create#view')
 			->setRequirement('itemID', '\d+')
 			->setMethod('GET');
-		$router['ms.user.account']->add('ms.user.return.store', '/returns/store/{itemID}', '::Controller:OrderReturn:Account:Create#store')
+		$router['ms.user.account']->add('ms.user.return.store', '/store/{itemID}', '::Controller:OrderReturn:Account:Create#store')
 			->setRequirement('itemID', '\d+')
 			->setMethod('POST');
 	}
