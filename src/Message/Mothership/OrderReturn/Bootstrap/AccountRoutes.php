f14a9e3e53c32d4bb6efed20a48d59add0d13fd8
Laurence Roberts <lsjroberts@gmail.com>
2013-08-29 16:56:09 +0100

    Removed account returns listing

--- src/Message/Mothership/OrderReturn/Bootstrap/AccountRoutes.php
+++ src/Message/Mothership/OrderReturn/Bootstrap/AccountRoutes.php
@@ -8,7 +8,6 @@ class AccountRoutes implements RoutesInterface
 {
 	public function registerRoutes($router)
 	{
-		$router['ms.user.account']->add('ms.user.return.listing', '/returns', '::Controller:OrderReturn:Account:Listing#view');
 		$router['ms.user.account']->add('ms.user.return.detail', '/returns/view/{returnID}', '::Controller:OrderReturn:Account:Detail#view')
 			->setRequirement('returnID', '\d+');
 
