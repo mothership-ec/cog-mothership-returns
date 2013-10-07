bcf80f8e390a269c9129643a6efa439e85c65f46
Laurence Roberts <lsjroberts@gmail.com>
2013-10-07 16:55:15 +0100

    Added returns tab listener to order tabs

--- src/Message/Mothership/OrderReturn/Bootstrap/EventListener.php
+++ src/Message/Mothership/OrderReturn/Bootstrap/EventListener.php
@@ -6,6 +6,8 @@ use Message\Cog\Event\SubscriberInterface;
 use Message\Cog\Event\Event;
 use Message\Cog\Event\EventListener as BaseListener;
 use Message\Mothership\ControlPanel\Event\BuildMenuEvent;
+use Message\Mothership\Commerce\Order\Events as OrderEvents;
+use Message\Mothership\Commerce\Order\Event\BuildOrderTabsEvent;
 
 /**
  * Event listener for the OrderReturn component.
@@ -20,11 +22,24 @@ class EventListener extends BaseListener implements SubscriberInterface
 			BuildMenuEvent::BUILD_MAIN_MENU => array(
 				'registerMainMenuItems'
 			),
+			OrderEvents::BUILD_ORDER_TABS => array(
+				'registerTabItems'
+			),
 		);
 	}
 
 	public function registerMainMenuItems(BuildMenuEvent $event)
 	{
 		$event->addItem('ms.commerce.return.dashboard', 'Returns', array('ms.returns'));
+	}
+
+	/**
+	 * Register items to the sidebar of the orders-pages.
+	 *
+	 * @param BuildMenuEvent $event The event
+	 */
+	public function registerTabItems(BuildOrderTabsEvent $event)
+	{
+		$event->addItem('ms.commerce.order.view.return', 'ms.commerce.return.listing-title');
 	}
 }
