fcb69491086e3efd3844c35375f9fb3c1168ed01
Laurence Roberts <lsjroberts@gmail.com>
2013-10-31 20:22:46 +0000

    Added js to show/hide payment method

---
+++ resources/assets/js/details.js
@@ -0,0 +1,22 @@
+$(function()
+{
+	var toggleMethodInput = function()
+	{
+		var val;
+		val = $('#form_payee input[name="form[payee]"]:checked').val();
+
+		if ('client' == val || 'none' == val) {
+			$('#form_refund_method').parents('.field-wrap').hide(0);
+		}
+		else {
+			$('#form_refund_method').parents('.field-wrap').show(0);
+		}
+	};
+
+	$('#form_payee input[name="form[payee]"]').change(function()
+	{
+		toggleMethodInput();
+	});
+
+	toggleMethodInput();
+});
