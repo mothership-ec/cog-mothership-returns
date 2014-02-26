acad940ecf62e4c8ac1667ee4f28caf81759cda7
Laurence Roberts <lsjroberts@gmail.com>
2014-02-26 11:10:42 +0000

    Fixed return item create with orderItem fields

--- src/Create.php
+++ src/Create.php
@@ -81,6 +81,38 @@ class Create
 			'createdBy' => $return->authorship->createdBy(),
 		]);
 
+		// Get the values for the return item
+		$returnItemValues = [
+			'createdAt'      => $return->authorship->createdAt(),
+			'createdBy'      => $return->authorship->createdBy(),
+			'returnID'       => $returnResult->id(),
+			'orderID'        => ($return->item->order) ? $return->item->order->id : null,
+			'orderItemID'    => ($return->item->orderItem) ? $return->item->orderItem->id : null,
+			'exchangeItemID' => ($return->item->exchangeItem) ? $return->item->exchangeItem->id : null,
+			'noteID'         => ($return->item->note) ? $return->item->note->id : null,
+			'statusCode'     => Statuses::AWAITING_RETURN,
+			'reason'         => $return->item->reason,
+			'resolution'     => $return->item->resolution,
+			'balance'        => $return->item->balance,
+		];
+
+		// Merge in the order item fields, from the orderItem if it is set or
+		// just the item object.
+		$orderItemFields = [
+			'listPrice', 'net', 'discount', 'tax', 'gross', 'rrp', 'taxRate',
+			'productTaxRate', 'taxStrategy', 'productID', 'productName',
+			'unitID', 'unitRevision', 'sku', 'barcode', 'options', 'brand',
+			'weight'
+		];
+
+		foreach ($orderItemFields as $field) {
+			if ($return->item->orderItem and (!property_exists($return->item, $field) or !$return->item->$field)) {
+				$returnItemValues[$field] = $return->item->orderItem->$field;
+			} else {
+				$returnItemValues[$field] = $return->item->$field;
+			}
+		}
+
 		// Create the return item
 		$itemResult = $this->_query->run("
 			INSERT INTO
@@ -96,20 +128,26 @@ class Create
 				status_code        = :statusCode?i,
 				reason             = :reason?s,
 				resolution         = :resolution?s,
-				calculated_balance = :balance?f
-		", [
-			'createdAt'      => $return->authorship->createdAt(),
-			'createdBy'      => $return->authorship->createdBy(),
-			'returnID'       => $returnResult->id(),
-			'orderID'        => ($return->item->order) ? $return->item->order->id : null,
-			'orderItemID'    => ($return->item->orderItem) ? $return->item->orderItem->id : null,
-			'exchangeItemID' => ($return->item->exchangeItem) ? $return->item->exchangeItem->id : null,
-			'noteID'         => ($return->item->note) ? $return->item->note->id : null,
-			'statusCode'     => Statuses::AWAITING_RETURN,
-			'reason'         => $return->item->reason,
-			'resolution'     => $return->item->resolution,
-			'balance'        => $return->item->balance,
-		]);
+				calculated_balance = :balance?f,
+				list_price         = :listPrice?f,
+				net                = :net?f,
+				discount           = :discount?f,
+				tax                = :tax?f,
+				gross              = :gross?f,
+				rrp                = :rrp?f,
+				tax_rate           = :taxRate?f,
+				product_tax_rate   = :productTaxRate?f,
+				tax_strategy       = :taxStrategy?s,
+				product_id         = :productID?i,
+				product_name       = :productName?s,
+				unit_id            = :unitID?i,
+				unit_revision      = :unitRevision?s,
+				sku                = :sku?s,
+				barcode            = :barcode?s,
+				options            = :options?s,
+				brand              = :brand?s,
+				weight_grams       = :weight?i
+		", $returnItemValues);
 
 		// Re-load the return to ensure it is ready to be passed to the return
 		// slip file factory, and to be returned from the method.
