4839d53d351fdaac31d06e2ba4567bf24b12d91f
Laurence Roberts <lsjroberts@gmail.com>
2014-05-09 10:30:10 +0100

    Added requirements to balance routes

--- src/Bootstrap/BalanceRoutes.php
+++ src/Bootstrap/BalanceRoutes.php
@@ -11,10 +11,16 @@ class BalanceRoutes implements RoutesInterface
 		$router['ms.ecom.return']->setPrefix('/return/balance');
 
 		$router['ms.ecom.return']->add('ms.ecom.return.balance.process', '/{returnID}', 'Message:Mothership:OrderReturn::Controller:OrderReturn:Balance:Confirm#process')
-			->setMethod('POST');
-		$router['ms.ecom.return']->add('ms.ecom.return.balance', '/{returnID}', 'Message:Mothership:OrderReturn::Controller:OrderReturn:Balance:Confirm#index');
+			->setMethod('POST')
+			->setRequirement('returnID', '\d+');
+		$router['ms.ecom.return']->add('ms.ecom.return.balance', '/{returnID}', 'Message:Mothership:OrderReturn::Controller:OrderReturn:Balance:Confirm#index')
+			->setRequirement('returnID', '\d+');
 
-		$router['ms.ecom.return']->add('ms.ecom.return.balance.successful', '/{returnID}/successful/{hash}', 'Message:Mothership:OrderReturn::Controller:OrderReturn:Balance:Complete#successful');
-		$router['ms.ecom.return']->add('ms.ecom.return.balance.unsuccessful', '/{returnID}/unsuccessful/{hash}', 'Message:Mothership:OrderReturn::Controller:OrderReturn:Balance:Complete#unsuccessful');
+		$router['ms.ecom.return']->add('ms.ecom.return.balance.successful', '/{returnID}/successful/{hash}', 'Message:Mothership:OrderReturn::Controller:OrderReturn:Balance:Complete#successful')
+			->setRequirement('returnID', '\d+')
+			->setRequirement('hash', '.+');
+		$router['ms.ecom.return']->add('ms.ecom.return.balance.unsuccessful', '/{returnID}/unsuccessful/{hash}', 'Message:Mothership:OrderReturn::Controller:OrderReturn:Balance:Complete#unsuccessful')
+			->setRequirement('returnID', '\d+')
+			->setRequirement('hash', '.+');
 	}
 }
