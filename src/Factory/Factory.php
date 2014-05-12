2b946baaad0fb8f01ea9d6251ee340951dde63e1
Laurence Roberts <lsjroberts@gmail.com>
2014-05-12 14:15:41 +0100

    Added docs and addNote method

--- src/Factory/Factory.php
+++ src/Factory/Factory.php
@@ -9,6 +9,7 @@ use Message\Mothership\OrderReturn\Entity\OrderReturn;
 use Message\Mothership\OrderReturn\Entity\OrderReturnItem;
 use Message\Mothership\Commerce\Product\Unit\Unit as ProductUnit;
 use Message\Mothership\Commerce\Order\Entity\Item\Item as OrderItem;
+use Message\Mothership\Commerce\Order\Entity\Note\Note as OrderNote;
 
 /**
  * Factory for creating returns.
@@ -20,10 +21,28 @@ use Message\Mothership\Commerce\Order\Entity\Item\Item as OrderItem;
  */
 class Factory
 {
+	const NOTE_RAISED_FROM_RETURN = 'return';
+
+	/**
+	 * The return instance being assembled.
+	 *
+	 * @var OrderReturn
+	 */
 	protected $_return;
+
+	/**
+	 * The return item instance being added to the return.
+	 *
+	 * @var OrderReturnItem
+	 */
 	protected $_returnItem;
+
+	/**
+	 * The exchange item instance being added to the return item.
+	 *
+	 * @var OrderItem
+	 */
 	protected $_exchangeItem;
-	protected $_note;
 
 	/**
 	 * Set the return to use in the factory.
@@ -60,8 +79,7 @@ class Factory
 		$isProductUnit = ($item instanceof ProductUnit);
 
 		if (! $isOrderItem and ! $isProductUnit) {
-			// @todo Improve this message
-			throw new InvalidArgumentException("Must be an instance of Unit or Item");
+			throw new InvalidArgumentException("You can only set a return item from an OrderItem or ProductUnit");
 		}
 
 		if ($isOrderItem) {
@@ -108,22 +126,22 @@ class Factory
 		$returnItem = new OrderReturnItem;
 
 		// @todo Get the correct currency id from somewhere
-		$returnItem->listPrice = $unit->getPrice('retail', $currencyID);
-		$returnItem->rrp       = $unit->getPrice('rrp', $currencyID);
+		$returnItem->listPrice         = $unit->getPrice('retail', $currencyID);
+		$returnItem->rrp               = $unit->getPrice('rrp', $currencyID);
 
-		$returnItem->productTaxRate = (float) $unit->product->taxRate;
-		$returnItem->taxStrategy    = $unit->product->taxStrategy;
-		$returnItem->productID      = $unit->product->id;
-		$returnItem->productName    = $unit->product->name;
-		$returnItem->unitID         = $unit->id;
-		$returnItem->unitRevision   = $unit->revisionID;
-		$returnItem->sku            = $unit->sku;
-		$returnItem->barcode        = $unit->barcode;
-		$returnItem->options        = implode($unit->options, ', ');
-		$returnItem->brand          = $unit->product->brand;
-		$returnItem->weight         = (int) $unit->weight;
+		$returnItem->productTaxRate    = (float) $unit->product->taxRate;
+		$returnItem->taxStrategy       = $unit->product->taxStrategy;
+		$returnItem->productID         = $unit->product->id;
+		$returnItem->productName       = $unit->product->name;
+		$returnItem->unitID            = $unit->id;
+		$returnItem->unitRevision      = $unit->revisionID;
+		$returnItem->sku               = $unit->sku;
+		$returnItem->barcode           = $unit->barcode;
+		$returnItem->options           = implode($unit->options, ', ');
+		$returnItem->brand             = $unit->product->brand;
+		$returnItem->weight            = (int) $unit->weight;
 
-		$returnItem->returnedValue = null;
+		$returnItem->returnedValue     = null;
 		$returnItem->calculatedBalance = null;
 
 		$this->_returnItem = $returnItem;
@@ -141,17 +159,36 @@ class Factory
 	public function setReason(Collection\Item $reason)
 	{
 		if (! $this->_returnItem) {
-			throw new LogicException("You must first call setReturnItem() before setReason()");
+			throw new LogicException("You can not set a reason without having previously set a return item");
 		}
 
-		$this->_returnItem->reason = $reason->code;
+		$this->_returnItem->reason = $reason;
 
 		return $this;
 	}
 
+	/**
+	 * Set the note for the return. Attach the order if this is not a
+	 * standalone return and apply the default values.
+	 *
+	 * @param  OrderNote $note
+	 * @return Factory
+	 */
 	public function setNote(OrderNote $note)
 	{
-		// ...
+		if ($this->_returnItem->order) {
+			$note->order = $this->_returnItem->order;
+		}
+
+		if (! $note->raisedFrom) {
+			$note->raisedFrom = static::NOTE_RAISED_FROM_RETURN;
+		}
+
+		if (null === $note->customerNotified) {
+			$note->customerNotified = 0;
+		}
+
+		$this->_returnItem->note = $note;
 
 		return $this;
 	}
