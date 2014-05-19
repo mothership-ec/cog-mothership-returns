198c06f6cf00e943269a610503d6e59b09440815
Laurence Roberts <lsjroberts@gmail.com>
2014-05-19 13:21:56 +0100

    Added CreateReturnPaymentTable migration

---
+++ resources/migrations/_1400496399_CreateReturnPaymentTable.php
@@ -0,0 +1,24 @@
+<?php
+
+use Message\Cog\Migration\Adapter\MySQL\Migration;
+
+class _1400496399_CreateReturnPaymentTable extends Migration
+{
+	public function up()
+	{
+		$this->run("
+			CREATE TABLE `return_payment` (
+				`return_id` int(11) unsigned NOT NULL,
+				`payment_id` int(11) unsigned NOT NULL,
+				PRIMARY KEY (`return_id`, `payment_id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+		");
+	}
+
+	public function down()
+	{
+		$this->run("
+			DROP `return_payment`
+		");
+	}
+}
