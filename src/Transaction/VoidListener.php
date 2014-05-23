071efba9b8d90c97676f5399c5cd4cd2b79e24c0
Joe Holdcroft <joe@message.co.uk>
2014-05-23 17:10:21 +0100

    Finish void functionality for returns

--- src/Transaction/VoidListener.php
+++ src/Transaction/VoidListener.php
@@ -3,6 +3,7 @@
 namespace Message\Mothership\OrderReturn\Transaction;
 
 use Message\Mothership\OrderReturn\Entity\OrderReturn;
+use Message\Mothership\OrderReturn\Statuses;
 
 use Message\Mothership\Commerce\Order\Transaction as OrderTransaction;
 
@@ -23,11 +24,13 @@ class VoidListener extends BaseListener implements SubscriberInterface
 	 */
 	static public function getSubscribedEvents()
 	{
-		return array(
-			OrderTransaction\Events::VOID => array(
-				array('deleteReturns'),
-			),
-		);
+		return [
+			OrderTransaction\Events::VOID => [
+				['deleteReturns'],
+				['removeReturnedItemFromDestinationStock'],
+				['revertReturnedItemStatus'],
+			],
+		];
 	}
 
 	/**
@@ -44,6 +47,93 @@ class VoidListener extends BaseListener implements SubscriberInterface
 
 		foreach ($transaction->records->getByType(OrderReturn::RECORD_TYPE) as $item) {
 			$delete->delete($item);
+		}
+	}
+
+	/**
+	 * Remove returned item from the stock detination it was put into when the
+	 * transaction is voided.
+	 *
+	 * @param OrderTransaction\Event\TransactionalEvent $event
+	 */
+	public function removeReturnedItemFromDestinationStock(OrderTransaction\Event\TransactionalEvent $event)
+	{
+		$transaction = $event->getTransaction();
+
+		// Skip if the transaction is not a return transaction
+		if (Types::ORDER_RETURN !== $transaction->type) {
+			return false;
+		}
+
+		$stockManager = $this->get('stock.manager');
+
+		$stockManager->setTransaction($event->getDbTransaction());
+		$stockManager->createWithRawNote(true);
+
+		$stockManager->setReason($this->get('stock.movement.reasons')->get('void_transaction'));
+
+		$event->getDbTransaction()->add("
+			SET @STOCK_NOTE = CONCAT('Void transaction #', ?i)
+		", $transaction->id);
+
+		$stockManager->setNote('@STOCK_NOTE');
+		$stockManager->setAutomated(true);
+
+		foreach ($transaction->records->getByType(OrderReturn::RECORD_TYPE) as $return) {
+			// Skip if the return is not completed (stock will not have been added)
+			if (Statuses::RETURN_COMPLETED !== $return->item->status->code) {
+				return false;
+			}
+
+			$stockManager->decrement($return->item->unit, $return->item->returnedStockLocation);
+		}
+	}
+
+	/**
+	 * Update the status of the returned item if there is one to whatever the
+	 * status was before it was returned.
+	 *
+	 * Currently this just chooses the status before the current one, but it
+	 * could be more robust.
+	 *
+	 * @todo Look into making this more robust, by knowing how many steps to
+	 *       take back in the status history to get to just before the return
+	 *       started.
+	 *
+	 * @param  OrderTransaction\Event\TransactionalEvent $event
+	 */
+	public function revertReturnedItemStatus(OrderTransaction\Event\TransactionalEvent $event)
+	{
+		$transaction = $event->getTransaction();
+
+		// Skip if the transaction is not a return transaction
+		if (Types::ORDER_RETURN !== $transaction->type) {
+			return false;
+		}
+
+		$itemEdit = $this->get('order.item.edit');
+
+		$itemEdit->setTransaction($event->getDbTransaction());
+
+		foreach ($transaction->records->getByType(OrderReturn::RECORD_TYPE) as $return) {
+			// Skip if the return was not for an order (i.e. it was a standalone return)
+			if (!$return->item->order) {
+				return false;
+			}
+
+			$item    = $return->item->orderItem;
+			$history = $this->get('order.item.status.loader')->getHistory($item);
+
+			// Skip if there was not more than 2 statuses on the item
+			if (count($history) < 2) {
+				continue;
+			}
+
+			// Get the status before the current one
+			$previousStatus = array_shift($history);
+			$previousStatus = array_shift($history);
+
+			$itemEdit->updateStatus($item, $previousStatus->code);
 		}
 	}
 }
