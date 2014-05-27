8dffaa1bf3712258234768ed805e04b5497b2433
Laurence Roberts <lsjroberts@gmail.com>
2014-05-27 13:05:40 +0100

    Added migration to update data to match current migration state
    Plus a small fix on the admin detail view

---
+++ resources/migrations/_1401184475_UpdateDataToMatchMigrationState.php
@@ -0,0 +1,70 @@
+<?php
+
+use Message\Cog\Migration\Adapter\MySQL\Migration;
+
+class _1401184475_UpdateDataToMatchMigrationState extends Migration
+{
+	/**
+	 * Updates old data to match the current migration state.
+	 *
+	 * - returned_value from order_item.actual_price
+	 * - remaining_balance from balance
+	 * - calculated_balance from order_item.actual_price - exchange_item.actual_price
+	 * - balance from calculated_balance
+	 * - actual_price from order_item.actual_price
+	 * - completed_at from order_item_status.created_at (for 2200)
+	 * - completed_by from order_item_status.created_by (for 2200)
+	 */
+	public function up()
+	{
+		// remaining_balance from balance
+		$this->run("
+			UPDATE
+				return_item
+			SET
+				remaining_balance = balance
+		");
+
+		// returned_value from order_item.actual_price
+		// calculated_balance from order_item.actual_price - exchange_item.actual_price
+		// actual_price from order_item.actual_price
+		// balance from calculated_balance
+		// actual_price from order_item.actual_price
+		$this->run("
+			UPDATE
+				return_item ri
+			LEFT JOIN
+				order_item oi ON (ri.item_id = oi.item_id)
+			LEFT JOIN
+				order_item ei ON (ri.exchange_item_id = ei.item_id)
+			SET
+				ri.returned_value     = oi.actual_price,
+				ri.calculated_balance = oi.actual_price - IFNULL(ei.actual_price, 0),
+				ri.actual_price       = oi.actual_price,
+				ri.balance            = CASE
+					WHEN ri.balance IS NOT NULL THEN oi.actual_price - IFNULL(ei.actual_price, 0)
+					ELSE ri.balance = ri.balance
+				END,
+				ri.actual_price       = oi.actual_price
+		");
+
+		// completed_at from order_item.status.created_at (for 2200)
+		// completed_by from order_item.status.created_by (for 2200)
+		$this->run("
+			UPDATE
+				return_item ri
+			LEFT JOIN
+				order_item_status ois ON (ri.item_id = ois.item_id AND ois.status_code = 2200)
+			SET
+				ri.completed_at = ois.created_at,
+				ri.completed_by = ois.created_by
+		");
+	}
+
+	public function down()
+	{
+		$this->run("
+
+		");
+	}
+}
