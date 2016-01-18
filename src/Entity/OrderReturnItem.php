c354dd59636752f538578d43d3892e1949394d6f
thomasjthomasj <thomasjmarchant@hotmail.com>
2016-01-18 14:06:56 +0000

    remove unused method

--- src/Entity/OrderReturnItem.php
+++ src/Entity/OrderReturnItem.php
@@ -113,21 +113,6 @@ class OrderReturnItem
 		return $this->balance !== null;
 	}
 
-	public function toBeReconciled()
-	{
-		if ($this->hasBalance()) {
-			return true;
-		}
-
-		if ($this->payeeIsCustomer()) {
-
-		}
-
-		if ($this->payeeIsRetailer()) {
-
-		}
-	}
-
 	public function hasCalculatedBalance()
 	{
 		return $this->calculatedBalance != 0;
